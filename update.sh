f21ad64d81e557cf324e684e5d5cc0ed4743ebc0
Mrlinkerrorsystem <54057540+ahmadchen@users.noreply.github.com>
2019-11-15 22:05:39 +0700

    Add files via upload

---
+++ update.sh
@@ -0,0 +1,54 @@
+#
+#  _    _            _               _____           
+# | |  | |          | |             |  __ \          
+# | |__| | __ _  ___| | _____ _ __  | |__) | __ ___  
+# |  __  |/ _` |/ __| |/ / _ \ '__| |  ___/ '__/ _ \ 
+# | |  | | (_| | (__|   <  __/ |    | |   | | | (_) |
+# |_|  |_|\__,_|\___|_|\_\___|_|    |_|   |_|  \___/ 
+#          Hacking Tools TEAM
+
+clear
+
+sudo chmod +x /etc/
+
+clear
+
+sudo chmod +x /usr/share/doc
+
+clear
+
+sudo rm -rf /usr/share/doc/tools-Hacking/
+
+clear
+
+cd /etc/
+
+clear
+
+sudo rm -rf /etc/TeamHackerCyberArmyIndonesia
+
+clear
+
+mkdir TeamHackerCyberArmyIndonesia
+
+clear
+
+cd TeamHackerCyberArmyIndonesia
+
+clear
+
+git clone https://github.com/TeamHackerCyberArmyIndonesia/tools-Hacking
+
+clear
+
+cd tools-Hacking
+
+clear
+
+sudo chmod +x install.sh
+
+clear
+
+./install.sh
+
+clear
